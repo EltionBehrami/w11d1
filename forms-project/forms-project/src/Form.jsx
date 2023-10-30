173a1e137a81d722f4e0d6cd33617d79bed197c4
“Eltion <ebehrami01@gmail.com>
2023-10-30 13:58:33 -0400

    started form component

---
+++ forms-project/forms-project/src/Form.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+
+function Form() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState(""); 
+    const [phone, setPhone] = useState(""); 
+    const [phoneType, setPhoneType] = useState(); 
+    const [staff, setStaff] = useState();
+    const [bio, setBio] = useState(); 
+    const [signUp, setSignUp] = useState();
+
+
+    
+    
+
+
+
+}
