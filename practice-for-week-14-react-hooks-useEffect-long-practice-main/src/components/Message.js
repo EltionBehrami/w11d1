241df95c3e74a9b00b5f45b46cd7a60aa601ea6a
“Eltion <ebehrami01@gmail.com>
2023-10-30 17:05:02 -0400

    finished phase 4

--- practice-for-week-14-react-hooks-useEffect-long-practice-main/src/components/Message.js
+++ practice-for-week-14-react-hooks-useEffect-long-practice-main/src/components/Message.js
@@ -4,9 +4,20 @@ import { useEffect, useState } from "react";
 
 
 
-function Message({ size }) {
+function Message({ size, featherCount }) {
   
   const [sizeClass, setSizeClass] = useState('');
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    if (featherCount <= 0)
+      setMessage('Oh my! Your bird is naked!');
+    else if (featherCount >= 10) {
+      setMessage('Full turkey!');
+    } else {
+      setMessage('Coming along...');
+    }
+  }, [featherCount])
 
   useEffect(() => {
     console.log('Message size', size);
@@ -31,7 +42,7 @@ function Message({ size }) {
 
   return (
     <div className={`message ${sizeClass}`}>
-      (Oh my! Your bird is naked!)
+     {message}
     </div>
   );
 };
